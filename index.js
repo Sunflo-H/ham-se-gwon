dce3acb81875f9efd24cae1bf29e7c11b303d70a
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2022-04-21 01:19:08 +0900

    promise 학습

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
-console.log(kakao);
+const inputButton = document.querySelector('input[type=button]');
 
-
-var infowindow = new kakao.maps.InfoWindow({zIndex:1});
-
+let address = '';
+let data = '';
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = {
         center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
         level: 3, // 지도의 확대 레벨
-        // mapTypeId : new kakao.maps.mapTypeId.SKYVIEW
-    };  
+        // mapTypeId : kakao.maps.MapTypeId.SKYVIEW
+};
 
-// 지도를 생성합니다    
+// 지도를 생성합니다
 var map = new kakao.maps.Map(mapContainer, mapOption); 
 
+var geocoder = new kakao.maps.services.Geocoder();
+console.log(geocoder);
 
-function sample6_execDaumPostcode() {
+async function slow() {
+    return new Promise((resolve,reject) => {
+        geocoder.addressSearch('서울 광진구 구의동', callback); // 함수 실행    
+    })
+}
+
+function callback(result, status) {
+    if(status === kakao.maps.services.Status.OK) {
+        console.log(result);
+        data = result;
+        console.log(data);
+    }
+}
+
+function setMapType(maptype) {
+     map.setMapTypeId(kakao.maps.MapTypeId[maptype]);
+}
+slow().then(() => {
+    
+});
+console.log(data);
+
+
+function addressSearchPopUp() {
     new daum.Postcode({
         oncomplete: function (data) {
             // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
@@ -22,44 +46,18 @@ function sample6_execDaumPostcode() {
             // 각 주소의 노출 규칙에 따라 주소를 조합한다.
             // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
             var addr = ''; // 주소 변수
-            var extraAddr = ''; // 참고항목 변수
-            console.log(addr);
             //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
             if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
                 addr = data.roadAddress;
             } else { // 사용자가 지번 주소를 선택했을 경우(J)
                 addr = data.jibunAddress;
             }
-            console.log(addr);
-
-            // 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
-            if (data.userSelectedType === 'R') {
-                // 법정동명이 있을 경우 추가한다. (법정리는 제외)
-                // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
-                if (data.bname !== '' && /[동|로|가]$/g.test(data.bname)) {
-                    extraAddr += data.bname;
-                }
-                // 건물명이 있고, 공동주택일 경우 추가한다.
-                if (data.buildingName !== '' && data.apartment === 'Y') {
-                    extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
-                }
-                // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
-                if (extraAddr !== '') {
-                    extraAddr = ' (' + extraAddr + ')';
-                }
-                // 조합된 참고항목을 해당 필드에 넣는다.
-                document.getElementById("sample6_extraAddress").value = extraAddr;
-
-            } else {
-                document.getElementById("sample6_extraAddress").value = '';
-            }
-
-            // 우편번호와 주소 정보를 해당 필드에 넣는다.
-            document.getElementById('sample6_postcode').value = data.zonecode;
-            document.getElementById("sample6_address").value = addr;
-            console.log(addr);
-            // 커서를 상세주소 필드로 이동한다.
-            document.getElementById("sample6_detailAddress").focus();
+            address = addr;
+            console.log(address);
         }
     }).open();
 }
+
+inputButton.addEventListener('click', () => {
+    
+})
