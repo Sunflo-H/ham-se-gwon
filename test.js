a4b54aef0382f6a9a044a199d602ff3a02dfe505
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2022-05-08 15:40:40 +0900

    검색창 z인덱스 조절

--- test.js
+++ test.js
@@ -1,19 +1,51 @@
+const ul = document.querySelector(".pop_rel_keywords");
+const searchInput = document.querySelector(".search_input");
+const relContainer = document.querySelector(".rel_search");
 
+const checkInput = () => {
+    const beforeInput = searchInput.value;
+    timer(beforeInput);
+}
 
-const input = document.querySelector('input[type=text]');
+const timer = (beforeInput) => {
+    setTimeout(() => {
 
-console.log(input);
+        if (searchInput.value === beforeInput) {
+            console.log("입력멈춤");
+            loadData(searchInput.value);		// 0.5초 내에 입력창이 변했다면 데이터 로드
+            checkInput();
 
-$(input).on("propertychange change paste input",function(e) {
-    fetch(`https://dapi.kakao.com/v2/local/search/address.json?query=${e.target.value}&size=5`, {
-    headers: { Authorization: `KakaoAK 621a24687f9ad83f695acc0438558af2` }
-    })
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.log("error:" + error));
-});
+        } else {
+            console.log("입력변함");
+            checkInput();
+        }
 
-// input.addEventListener('change', e => {
-//     console.log(e.target);
-//     console.log(e.target.value);
-// })
+        if (searchInput.value === "") {		// 입력창이 비었다면 추천 검색어 리스트 숨김
+            relContainer.classList.add("hide");
+        } else {
+            relContainer.classList.remove("hide");
+        }
+    }, 500);
+}
+
+const loadData = (input) => {
+    const url = `https://completion.amazon.com/... 중략...prefix=${input}&event=onFocusWithSearchT`;
+    // 매개변수 input 값에 따라 서버에서 해당 검색어와 연관된 추천검색어가 담긴 데이터가 불러와진다.
+
+    if (cache === url) return;	// 이전에 부른 데이터랑 다를 때만 fetch로 데이터를 새로 불러온다.
+    else {
+        cache = url;
+        fetch(url)
+            .then((res) => res.json())
+            .then(res => fillSearch(res.suggestions))
+    }
+}
+
+const fillSearch = (suggestArr) => {
+    ul.innerHTML = "";
+    suggestArr.forEach((el, idx) => {
+      const li = document.createElement("li");
+      li.innerHTML = el.value;
+      ul.appendChild(li);
+    }) 
+  }
