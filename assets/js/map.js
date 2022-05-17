a839b67b846a7718714ee0bb8d1742a233f25af0
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2022-05-17 23:45:07 +0900

    keyword

--- assets/js/map.js
+++ assets/js/map.js
@@ -7,6 +7,7 @@ const body = document.querySelector('body');
 
 let historyList = [];
 
+let map;
 
 
 /**
@@ -14,22 +15,22 @@ let historyList = [];
  * setMarker({map, lat, lng}) : 좌표를 받아 마커를 지도에 띄움
  */
 function setMap(lat, lng) {
+    console.log(lat, lng);
     const mapContainer = document.getElementById('map'); // 지도를 표시할 div 
     let mapOption = {
         center: new kakao.maps.LatLng(lat, lng), // 지도의 중심좌표
         level: 3, // 지도의 확대 레벨
     };
-    let map = new kakao.maps.Map(mapContainer, mapOption);
-    let data = {
-        map: map,
+    map = new kakao.maps.Map(mapContainer, mapOption);
+    let coords = {
         lat: lat,
         lng: lng
     }
-    return data;
+    return coords;
 }
 
-function setMarker(data) {
-    const { map, lat, lng } = data;
+function setMarker(coords) {
+    const { lat, lng } = coords;
     var coords = new kakao.maps.LatLng(lat, lng);
     var marker = new kakao.maps.Marker({
         position: coords
@@ -43,7 +44,8 @@ function setMarker_addr() {
 
 }
 
-function getUserLocation(callback, event) {
+// 앱의 초기단계에서 사용자의 위치를 받는 함수
+function getUserLocation() {
     return new Promise((res, rej) => {
         navigator.geolocation.getCurrentPosition(res, rej);
     });
@@ -55,27 +57,22 @@ function findAddress(position) {
     setMap(lat, lng);
 }
 
-function findKeyword(position) {
-    const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
+function getKeywordList() {
+    // const { La, Ma } = map.getCenter();
+    // const lat = Ma;
+    // const lng = La;
+    // console.log(lat);
+    // console.log(lng);
 
-    fetch(`https://dapi.kakao.com/v2/local/search/keyword.json?y=${lat}&x=${lng}&radius=20000&query=롯데리아`, {
-        headers: { Authorization: `KakaoAK 621a24687f9ad83f695acc0438558af2` }
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            // setHtmlRelation(data);
-            // setHtmlHistory();
-            // if (data.documents.length !== 0 || historyList.length !== 0) {
-            //     openSearchBar();
-            // } else {
-            //     closeSearchBar();
-            // }
-            // openSearchBar_relation();
-            // openSearchBar_histroy();
-        })
-        .catch((error) => console.log("error:" + error));
+
+    // fetch(`https://dapi.kakao.com/v2/local/search/keyword.json?y=${lat}&x=${lng}&radius=20000&query=롯데리아`, {
+    //     headers: { Authorization: `KakaoAK 621a24687f9ad83f695acc0438558af2` }
+    // })
+    //     .then((response) => response.json())
+    //     .then((data) => {
+    //         console.log(data);
+    //     })
+    //     .catch((error) => console.log("error:" + error));
 }
 
 function error() {
@@ -83,11 +80,16 @@ function error() {
 }
 
 /**
- ** 검색 함수들 적용 순서
- * 값 입력 => getAddrList() or getRestList()로 데이터를 받아와 type와 name의 객체를 Promise로 리턴
- * 이렇게 얻은 Promise데이터를 연관검색어로 보여준다. setHtmlRelation()
+ * & 검색 함수들 적용 설명
+ * 값 입력 => getAddrList() or getRestList()로 데이터를 받아와 {type, name} 객체를 Promise로 리턴
+ * 이렇게 얻은 Promise데이터를 setHtmlRelation()함수를 실행하여 연관검색어로 보여준다. 
  * 
- * 연관검색어 클릭 => clickRelation() 실행 => 클릭한 데이터의 type에 따라 각각 함수실행 findCoordsByAddr(), findCoordsByKeyword()
+ * 연관검색어 클릭 => clickRelation() 실행 => 
+ * 클릭한 데이터의 {type : 주소 또는 키워드}에 따라 각각 함수실행 findCoordsByAddr(), findCoordsByKeyword()
+ * 
+ * type이 키워드인 경우
+ * 일단 현재 좌표 얻어와서 반경 내 검색
+ * 없으면 전체 검색
  */
 // ! 이 함수들 비동기함수로 변환될수 있대. 누르면 async await코드로 바뀜 공부하고 스스로 바꾸자
 function getAddrList(keyword) {
@@ -99,9 +101,9 @@ function getAddrList(keyword) {
             let list = [];
             //forEach 안의 data는 배열, data[0].address_name
             data.documents.forEach(data => {
-                let obj = { 
-                    type : "address" ,
-                    name : data.address_name
+                let obj = {
+                    type: "address",
+                    name: data.address_name
                 }
                 list.push(obj);
             })
@@ -117,8 +119,8 @@ function getRestList(keyword) {
             const restList = data.results[0].items.filter(restaurant => restaurant.name.substring(0, keyword.length) === keyword);
             restList.forEach(data => {
                 let obj = {
-                    type : "restaurant",
-                    name : data.name
+                    type: "restaurant",
+                    name: data.name
                 }
                 list.push(obj);
             })
@@ -129,7 +131,6 @@ function getRestList(keyword) {
 //* 검색창에 연관 검색어를 세팅하는 함수
 function setHtmlRelation(relationList) {
     const relationContainer = document.querySelector('.relation-container');
-    console.log(relationList);
     while (relationContainer.hasChildNodes()) {
         relationContainer.removeChild(relationContainer.firstChild);
     }
@@ -161,15 +162,15 @@ function setHtmlHistory() {
 
 //* 연관 단어를 클릭하면 바로 검색결과 나타나게 하는 함수
 function clickRelation(e) {
-    console.log("실행");
+    console.log("연관검색어 클릭");
     console.log(e.target);
     const relation = e.target.innerText;
     const type = e.target.getAttribute("data-type");
     const currentLocation = document.querySelector('.current-location');
 
     switch (type) {
-        case "address" : findCoordsByAddr(relation); break;
-        case "restaurant" : findCoordsByKeyword(relation); break;
+        case "address": findCoordsByAddr(relation); break;
+        case "restaurant": findCoordsByKeyword(relation); break;
     }
     currentLocation.innerText = relation;
 }
@@ -220,38 +221,55 @@ function findCoordsByAddr(addr) {
 }
 
 function findCoordsByKeyword(keyword) {
-    // 
-    // 
-    // 
+
     /**
      * 여러 데이터를 찾게 될텐데
      * 첫번째 데이터로 map.setCenter()를 정하고
      * 좌측에 모달만들어서 ~로 검색한 결과 글씨와 그 리스트를 띄워줘
      * 
      */
+    const { La, Ma } = map.getCenter();
+    const lat = Ma;
+    const lng = La;
+    fetch(`https://dapi.kakao.com/v2/local/search/keyword.json?y=${lat}&x=${lng}&radius=20000&query=${keyword}`, {
+        headers: { Authorization: `KakaoAK 621a24687f9ad83f695acc0438558af2` }
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((error) => console.log("error:" + error));
+
 
     // 장소 검색 객체를 생성합니다
     var ps = new kakao.maps.services.Places();
 
     // 키워드로 장소를 검색합니다
     ps.keywordSearch(keyword, placesSearchCB);
+
+    //! 키워드로 찾되 조건을 걸고싶어
+    //! center를 기준으로 주변에 있으면 주변꺼 보여주고
+    //! 주변에 없으면 전체로 검색
+    //! 어떤 키워드를 검색하든 주변을 우선으로 해야해
 }
 
 // 키워드 검색 완료 시 호출되는 콜백함수 입니다
 function placesSearchCB(data, status, pagination) {
     if (status === kakao.maps.services.Status.OK) {
+        //data = 객체 배열, 객체에서 x,y or address_name을 꺼내면 될듯
         console.log(data);
+
         // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
         // LatLngBounds 객체에 좌표를 추가합니다
         var bounds = new kakao.maps.LatLngBounds();
 
         for (var i = 0; i < data.length; i++) {
-            // displayMarker(data[i]);
+            displayMarker(data[i]);
             bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
         }
 
         // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
-        // map.setBounds(bounds);
+        map.setBounds(bounds);
     }
 }
 
@@ -259,7 +277,6 @@ function placesSearchCB(data, status, pagination) {
 function displayMarker(place) {
     // 마커를 클릭하면 장소명을 표출할 인포윈도우 입니다
     var infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
-
     // 마커를 생성하고 지도에 표시합니다
     var marker = new kakao.maps.Marker({
         map: map,
@@ -330,7 +347,7 @@ function openSearchBar_histroy() {
 
 
 
-// 주소로 검색 - 검색창에 값이 입력될 때마다 연관검색어, 히스토리를 보여주는 이벤트
+// 검색창에 값이 입력될 때마다 연관검색어, 히스토리를 보여주는 이벤트
 searchInput.addEventListener('input', e => {
     if (e.target.value === '') return; //value가 공백이 되면 query에러가 발생하여 넣은 코드
     // if (e.target.value ===)
@@ -351,26 +368,6 @@ searchInput.addEventListener('input', e => {
         }
 
     });
-    // fetch(`https://dapi.kakao.com/v2/local/search/address.json?query=${e.target.value}&size=5`, {
-    //     headers: { Authorization: `KakaoAK 621a24687f9ad83f695acc0438558af2` }
-    // })
-    //     .then((response) => response.json())
-    //     .then((data) => {
-    //         if (data.documents.length !== 0 || historyList.length !== 0) {
-    //             openSearchBar();
-    //         } else {
-    //             closeSearchBar();
-    //         }
-    //         openSearchBar_relation();
-    //         openSearchBar_histroy();
-    //         setHtmlRelation(data);
-    //         setHtmlHistory();
-    //     })
-    //     .catch((error) => console.log("error:" + error));
-})
-// 키워드로 검색 - 검색창에 값이 입력될 때마다 연관검색어, 히스토리를 보여주는 이벤트
-searchInput.addEventListener('input', e => {
-    getUserLocation(findKeyword, e);
 })
 
 searchInput.addEventListener('click', e => {
